03e8fe1024d02a6129000352951ec309f1892086
majdi <majdioa7sh@gmail.com>
2024-10-05 10:43:40 +0300

    gg911

--- backend/controllers/postsController.js
+++ backend/controllers/postsController.js
@@ -22,32 +22,33 @@ const { Comment } = require("../models/Comment");
 //!createPostCtrl
 module.exports.createPostCtrl = asyncHandler(async (req, res) => {
   // 1. Validation for image
-  // if (!req.file) {
-  //   return res.status(400).json({ message: "no image provided" });
-  // }
+  if (!req.file) {
+    return res.status(400).json({ message: "no image provided" });
+  }
   // 2. Validation for data
   const { error } = validateCreatePost(req.body);
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
   // 3. Upload photo
-  // const imagePath = path.join(__dirname, `../images/${req.file.filename}`);
-  // const result = await cloudinaryUploadImage(imagePath);
+  const imagePath = path.join(__dirname, `../images/${req.file.filename}`);
+  console.log(__dirname);
+  const result = await cloudinaryUploadImage(imagePath);
   // 4. Create new post and save it into DB
   const post = await Post.create({
     title: req.body.title,
     description: req.body.description,
     category: req.body.category,
     user: req.user.id,
-    // image: {
-    //   url: result.secure_url,
-    //   publicId: result.public_id,
-    // },
+    image: {
+      url: result.secure_url,
+      publicId: result.public_id,
+    },
   });
   // 5. Send response to client
   res.status(201).json(post);
   // 6. Remove image from server (VS Code)
-  //fs.unlinkSync(imagePath);
+  fs.unlinkSync(imagePath);
 });
 
 /**-------------------------------------------------
